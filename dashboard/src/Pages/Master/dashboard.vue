d7adbde833ed9f7bb8020baa0338120c2041e420
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-19 22:39:46 +0900

    updating dashboard page

---
+++ dashboard/src/Pages/Master/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-screen h-screen">
+        <div class="w-[200px] h-full bg-gray-200">
+            Side Bar
+        </div>
+    </div>
+    </template>
+    
+    <script>
+    export default{
+    
+    }
+    </script>
+    
+    <style>
+    
+    </style>
--- dashboard/src/Pages/home.vue
+++ dashboard/src/Pages/home.vue
@@ -1,5 +1,7 @@
 <template>
-
+<div>
+    This is my home
+</div>
 </template>
 
 <script>
