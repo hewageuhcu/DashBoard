f0aca294e7ff721ab84e902e184e5f0f150074d7
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-20 01:47:15 +0900

    tailwind css installed

--- dashboard/src/Pages/Master/MasterDashboard.vue
+++ dashboard/src/Pages/Master/MasterDashboard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-screen h-screen flex">
-        <div class="w-[200px] h-full bg-gray-200">
+        <div class="w-[400px] h-full bg-gray-200">
             Side Bar
         </div>
         <div class="w-full h-full bg-gray-400">
