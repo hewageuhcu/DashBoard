2cbc19ac971a2e21a9bbba397cc6fcfbf7579f7b
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-20 02:53:15 +0900

    🥳

--- dashboard/src/Pages/Master/MasterDashboard.vue
+++ dashboard/src/Pages/Master/MasterDashboard.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="w-screen h-screen flex">
-        <div class="w-[400px] h-full bg-gray-200">
-            Side Bar
+        <div class="w-[400px] h-full bg-gray-200 text-white">
+           <div class="h-[50px] bg-gray-900">
+Header Sidebar
+           </div>
+           <div class="h-[calc(100vh-50px)] bg-gray-800">
+            Menu
+           </div>
         </div>
+      
         <div class="w-full h-full bg-gray-400">
-            main
+            <div class="h-[50px] bg-gray-900">
+Header Sidebar
+           </div>
         </div>
     </div>
     </template>
