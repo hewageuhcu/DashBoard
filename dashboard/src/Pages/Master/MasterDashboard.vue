5e92f1bd21e19b375497d6c97920dffe280197f3
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-20 02:57:38 +0900

    🥳

--- dashboard/src/Pages/Master/MasterDashboard.vue
+++ dashboard/src/Pages/Master/MasterDashboard.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="w-screen h-screen flex">
         <div class="w-[400px] h-full bg-gray-200 text-white">
-           <div class="h-[50px] bg-gray-900">
-Header Sidebar
-           </div>
+           <div class="h-[50px] bg-gray-900 flex justify-start items-center">
+<div class="px-[20px]">
+            <h3 class="font-bold text-xl">Admin Dashboard</h3>
+        </div>   
+        </div>
            <div class="h-[calc(100vh-50px)] bg-gray-800">
             Menu
            </div>
@@ -12,6 +14,9 @@ Header Sidebar
         <div class="w-full h-full bg-gray-400">
             <div class="h-[50px] bg-gray-900">
 Header Sidebar
+           </div>
+           <div class="h-[calc(100vh-50px)] bg-white">
+
            </div>
         </div>
     </div>
