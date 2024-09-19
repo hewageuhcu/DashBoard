1a068013a09f491888f2ac65815b5d286fd486dc
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-20 03:38:54 +0900

    🥳

--- dashboard/src/Pages/Master/MasterDashboard.vue
+++ dashboard/src/Pages/Master/MasterDashboard.vue
@@ -8,7 +8,85 @@
         </div>
            <div class="h-[calc(100vh-50px)] bg-gray-800">
            <div class="px-[20px] flex flex-col">
-            <div>Home</div>
+            <br/>
+            <router-link 
+  to="/home" 
+  class="inline-flex items-center px-4 py-2 bg-gray-500 text-white rounded-lg shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-colors duration-300"
+>
+  <svg 
+    aria-hidden="true" 
+    class="mr-2 w-5 h-5 fill-current" 
+    viewBox="0 0 20 20" 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path 
+      fill-rule="evenodd" 
+      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0z" 
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+  HOME
+</router-link>
+<br/>
+<router-link 
+  to="/home" 
+  class="inline-flex items-center px-4 py-2 bg-gray-500 text-white rounded-lg shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-colors duration-300"
+>
+  <svg 
+    aria-hidden="true" 
+    class="mr-2 w-5 h-5 fill-current" 
+    viewBox="0 0 20 20" 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path 
+      fill-rule="evenodd" 
+      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0z" 
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+PROFILE
+</router-link>
+<br/>
+<router-link 
+  to="/home" 
+  class="inline-flex items-center px-4 py-2 bg-gray-500 text-white rounded-lg shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-colors duration-300"
+>
+  <svg 
+    aria-hidden="true" 
+    class="mr-2 w-5 h-5 fill-current" 
+    viewBox="0 0 20 20" 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path 
+      fill-rule="evenodd" 
+      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0z" 
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+  SETTINGS
+</router-link>
+<br/>
+<router-link 
+  to="/home" 
+  class="inline-flex items-center px-4 py-2 bg-gray-500 text-white rounded-lg shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-colors duration-300"
+>
+  <svg 
+    aria-hidden="true" 
+    class="mr-2 w-5 h-5 fill-current" 
+    viewBox="0 0 20 20" 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path 
+      fill-rule="evenodd" 
+      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0z" 
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+ HELP
+</router-link>
+
+
+
            </div>
            </div>
         </div>
