e807ed890afe975e50cbf9bdc9a8c93ca737e6c4
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-20 21:50:29 +0900

    search bar added

--- dashboard/src/Pages/Master/MasterDashboard.vue
+++ dashboard/src/Pages/Master/MasterDashboard.vue
@@ -70,6 +70,32 @@ DOWNLOAD
       <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96z"></path>
     </svg>
   </div>
+  <div class="w-[calc(100%-200px)] flex justify-center">
+    <form class="w-[calc(100%-200px)] flex justify-center">
+      <form class="flex items-center w-[500px]">
+  <label for="voice-search" class="sr-only">Search</label>
+  <div class="relative w-full">
+
+    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+      <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zm-6 4a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.817-4.817A6 6 0 012 8z" clip-rule="evenodd"></path>
+      </svg>
+    </div>
+    
+  
+    <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+    
+   
+    <router-link to="/" class="flex absolute inset-y-0 right-0 items-center pr-3">
+      <svg aria-hidden="true" class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0 5 5 0 01-5 5 1 1 0 102 0z" clip-rule="evenodd"></path>
+      </svg>
+    </router-link>
+  </div>
+</form>
+
+    </form>
+  </div>
 </div>
 
            <div class="h-[calc(100vh-50px)] bg-white">
@@ -81,6 +107,9 @@ Main
     
     <script>
     export default{
+    toggleSideBar(){
+
+    }
     
     }
     </script>
