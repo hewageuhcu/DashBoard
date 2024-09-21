11158979b3bdc6ab5c91956b333fb08ae243649a
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-21 15:56:46 +0900

    updating

--- dashboard/src/Pages/Master/MasterDashboard.vue
+++ dashboard/src/Pages/Master/MasterDashboard.vue
@@ -23,7 +23,7 @@
 <br/>
 
 <router-link 
-  to="/home" 
+  to="/Profile" 
   class="inline-flex items-center px-4 py-2 bg-gray-500 text-white rounded-lg shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-colors duration-300"
 >
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" class="w-6 h-6">
@@ -64,7 +64,7 @@ DOWNLOAD
         </div>
       
         <div class="w-full h-full bg-gray-400">
-          <div class="h-[50px] bg-gray-100 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b">
+          <div class="h-[50px] bg-gray-200 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b">
   <div class="cursor-pointer w-[30px]" @click="toggleSideBar">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-[25px] h-[25px]">
       <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96z"></path>
@@ -74,6 +74,7 @@ DOWNLOAD
     <form class="w-[calc(100%-200px)] flex justify-center">
       <form class="flex items-center w-[500px]">
   <label for="voice-search" class="sr-only">Search</label>
+  
   <div class="relative w-full">
 
     <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
